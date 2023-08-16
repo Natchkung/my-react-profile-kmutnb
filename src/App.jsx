34af29481e4fe3ba79331cd4a686780ae3b9793f
Natchkung <natchnon654321@gmail.com>
2023-08-17 01:42:28 +0700

    add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
 
 
 function App() {
   return (
     <>
+      <Header/>
       <h1 className="mt-4">Hello Wolrd</h1>
+      <h6 className='text-center'>test</h6>
     </>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../assets/header.css';
+
+function Header() {
+  return (
+    <div className='navbar'>Header</div>
+  )
+}
+
+export default Header
