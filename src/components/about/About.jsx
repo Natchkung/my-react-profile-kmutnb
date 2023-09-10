0d642a66dfe793cdf0305a4a8ffd16db5a53a82d
Natchkung <natchnon654321@gmail.com>
2023-09-11 03:28:20 +0700

    update

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -26,7 +26,66 @@ const About = () => {
         <h5 className='mt-4 text-center font-medium text-slate-500'>INTRODUCTION</h5>
             <h1 className='text-center'>ABOUT ME</h1>
           <div className="main-detail_me">
-          <h5 className='mt-4 text-center font-medium text-slate-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor nam minus magni placeat voluptate dolorum nobis, perferendis aperiam rem in modi magnam ratione dolores saepe velit voluptatum porro alias iusto maiores. Quos laudantium sint aperiam natus alias vel quod iusto, in cumque error ipsam a iure maiores magni tempora officia libero dicta totam laborum. Deserunt eaque totam sapiente iusto nulla perspiciatis inventore saepe a ratione aut, dolorum reiciendis ea voluptatem facilis nihil, adipisci quo dolorem iure dolores tempora harum id! Quasi nesciunt rem nemo, itaque consequatur, pariatur consectetur placeat molestias tempora sit dolores eveniet illum maxime tempore odio quisquam labore incidunt neque inventore debitis dolorem molestiae! Quae odio, vel aliquid vitae quos illo quisquam cum placeat pariatur veniam eligendi libero mollitia culpa! Sint nesciunt impedit ea odio exercitationem rerum placeat, ipsa pariatur magni dignissimos dolore? Eos id tempore perspiciatis quas ipsum alias saepe porro praesentium facere magni voluptatibus sequi, labore error suscipit earum molestias dignissimos nostrum. Earum odio deserunt saepe quia sint dicta sapiente excepturi reprehenderit atque ut quos dignissimos, nesciunt et harum sit optio perferendis dolor eaque sunt doloribus facilis error commodi architecto. Exercitationem, assumenda nesciunt. Unde explicabo debitis, dolore facere distinctio illum tenetur ad sint adipisci! Nostrum laudantium quas debitis vitae adipisci optio animi aut, quibusdam modi illum earum nam eum, fuga eligendi repellendus voluptatum cum iusto quasi autem commodi et. Rerum amet est facere, voluptatem id reiciendis vel praesentium tempora quo inventore fugiat nam consequuntur! Officia, ratione neque. Porro quos quam veniam non iusto sed impedit dignissimos, laborum accusamus repellendus perferendis necessitatibus animi ullam natus quaerat officia nemo fuga deleniti, aliquid recusandae. Quaerat dolor unde atque vel qui non dolorem temporibus repudiandae modi enim illum beatae repellat impedit sunt ullam tempora eum maxime ut, quis delectus eveniet cumque error. Mollitia nulla, quam quasi dolorem, maiores rerum magnam, quas autem recusandae necessitatibus suscipit! Eveniet debitis mollitia consequuntur architecto nostrum quidem corporis dolorem, iure voluptas veritatis. Odit, atque reiciendis? Vel commodi quam assumenda nostrum magnam maxime ipsum nihil id, recusandae vero nisi cum consectetur! Reprehenderit voluptates eaque explicabo molestiae adipisci, porro dignissimos dicta consequatur! Sapiente, deserunt! Hic, at! Rem distinctio architecto, qui dicta amet tempore ullam explicabo earum ex inventore consequatur ipsum neque dolorem aliquam eos excepturi iusto eius quo quaerat animi modi? Animi ea, unde hic natus quo quod odit eos ipsam vel quis? Deserunt minus vitae suscipit temporibus quaerat repellendus incidunt officia soluta, porro et maxime expedita?</h5>
+          <h5 className='mt-4 text-center font-medium text-slate-500'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum vel obcaecati possimus voluptatem quos commodi incidunt, exercitationem delectus iusto eligendi quasi quae, suscipit dignissimos ducimus soluta dolorum voluptatibus natus. Laborum beatae ullam iste deleniti ducimus delectus voluptatem aliquam reiciendis blanditiis! Non perspiciatis amet quibusdam in atque inventore! Veritatis, et ullam!</h5>
+			<div className="secon-detail">
+			<div>
+						<h3>ชื่อ-สกุล</h3>
+						<p>นาย ณัฐชานน ทรัพย์มีชัย</p>
+					</div>
+					<div>
+						<h3>ชื่อเล่น</h3>
+						<p>ณัฐ</p>
+					</div>
+					<div>
+						<h3>รหัสนักศึกษา</h3>
+						<p>6604062636151</p>
+					</div>
+					<div>
+						<h3>กำลังศึกษาอยู่ที่</h3>
+						<p>มหาวิทยาลัยเทคโนโลยีพระจอมเกล้าพระนครเหนือ (มจพ.)</p>
+					</div>
+					<div>
+						<h3>ที่อยู่ปัจจุบัน</h3>
+						<p>33/2 ถนนเทศบาล 8 ตำบล สระแก้ว อำเภอ เมืองสระแก้ว จังหวัดสระแก้ว รหัสไปรษณีย์ 27000</p>
+					</div>
+					<div>
+						<h3>จบการศึกษามัธยมปลายจาก</h3>
+						<p>โรงเรียนสระแก้ว</p>
+					</div>
+					<div>
+						<h3>เกรดเฉลี่ยมัธยมปลาย</h3>
+						<p>3.98</p>
+					</div>
+					<div>
+						<h3>วัน/เดือน/ปี ที่เกิด</h3>
+						<p>20 มีนาคม พ.ศ.2548</p>
+						<p>(<b>อายุ</b> 18 ปี)</p>
+					</div>
+					<div>
+						<h3>หมู่เลือด</h3>
+						<p>B (บี)</p>
+					</div>
+					<div>
+						<h3>สีที่ชอบ</h3>
+						<ul>
+							<li>ฟ้า/น้ำเงิน</li>
+							<li>เขียว</li>
+						</ul>
+					</div>
+					<div>
+						<h3>งานอดิเรก</h3>
+						<ul>
+							<li>ดู YouTube</li>
+							<li>เล่นเกม</li>
+							<li>ศึกษา เขียนเว็บไซต์</li>
+							<li>ศึกษา เขียนโปรแกรม</li>
+						</ul>
+					</div>
+					<div>
+						<h3>เป้าหมายในอนาคต</h3>
+						<p>Full Stack Developer และการเขียนโปรแกรมแขนงอื่น ๆ</p>
+					</div>
+			</div>
           </div>
         </div>
       </div>
