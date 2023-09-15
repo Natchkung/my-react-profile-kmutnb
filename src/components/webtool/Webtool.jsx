396d62fa155e06a5810235aa23ca5b7abd0e15f6
Natchkung <natchnon654321@gmail.com>
2023-09-16 03:27:24 +0700

    update

--- src/components/webtool/Webtool.jsx
+++ src/components/webtool/Webtool.jsx
@@ -13,14 +13,14 @@ function Webtool() {
 
   return (
     <section id="webtool">
-        <h5 className='mt-[-200px] text-center font-medium text-slate-500' data-aos="fade-up" data-aos-duration="1500">DEVELOPMENT TOOLS</h5>
-            <h1 className='text-center mb-8' data-aos="fade-down" data-aos-duration="1500">WEBSITE DEVELOPMENT TOOLS</h1>
+        <h5 className='mt-[-200px] text-center font-medium text-slate-500' data-aos="fade-up" data-aos-duration="3000">DEVELOPMENT TOOLS</h5>
+            <h1 className='text-center mb-8' data-aos="fade-down" data-aos-duration="3000">WEBSITE DEVELOPMENT TOOLS</h1>
         <div className="tech">
         <div className='flex flex-row flex-wrap justify-center gap-10'>
       {technologies.map((technology) => (
         <div className='w-28 h-28' key={technology.name}>
-          <div className="relative w-full h-full" data-aos="fade-right" data-aos-duration="1500"><BallCanvas icon={technology.icon}/></div>
-          <p className="text-center" data-aos="fade-left" data-aos-duration="1500">{technology.name}</p>
+          <div className="relative w-full h-full" data-aos="fade-right" data-aos-duration="3000"><BallCanvas icon={technology.icon}/></div>
+          <p className="text-center" data-aos="fade-left" data-aos-duration="3000">{technology.name}</p>
         </div>
       ))}
     </div>
