f48d03ebbe7506aab4e79b073f8b07bea0f3598c
Natchkung <natchnon654321@gmail.com>
2023-09-17 02:01:25 +0700

    update

--- src/components/videome/Videome.jsx
+++ src/components/videome/Videome.jsx
@@ -3,6 +3,7 @@ import './videome.css';
 import { motion } from "framer-motion";
 import Aos from 'aos';
 import 'aos/dist/aos.css';
+import ReactPlayer from 'react-player/youtube'
 
 const Videome = () => {
 
@@ -13,9 +14,18 @@ const Videome = () => {
 
   return (
     <section id="videome">
+      <div className="content-player">
       <h1 className='text-center' data-aos="fade-up" data-aos-duration="1500">My Video</h1>
       <div className="video mt-4" data-aos="fade-down" data-aos-duration="3000">
-      <iframe className="react-player rounded-2xl" width="1000" height="560" src="https://www.youtube.com/embed/N9Rk3d4fCJ8?si=ZS01d6Jk4ijPZmxa" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      <ReactPlayer
+          className='react-player'
+          controls
+          url='https://www.youtube.com/embed/N9Rk3d4fCJ8?si=ZS01d6Jk4ijPZmxa'
+          width='100%'
+          height='100%'
+        />
+      {/* <iframe className="react-player rounded-2xl" width="1000" height="560" src="https://www.youtube.com/embed/N9Rk3d4fCJ8?si=ZS01d6Jk4ijPZmxa" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> */}
+      </div>
       </div>
     </section>
   )
