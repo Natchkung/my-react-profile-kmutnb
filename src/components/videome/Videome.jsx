adc7cb97eb18c9afedcb73b664568c62e96f4822
Natchkung <natchnon654321@gmail.com>
2023-09-17 01:31:22 +0700

    update

--- src/components/videome/Videome.jsx
+++ src/components/videome/Videome.jsx
@@ -15,7 +15,7 @@ const Videome = () => {
     <section id="videome">
       <h1 className='text-center' data-aos="fade-up" data-aos-duration="1500">My Video</h1>
       <div className="video mt-4" data-aos="fade-down" data-aos-duration="3000">
-      <iframe className="react-player rounded-2xl" src="https://www.youtube.com/embed/N9Rk3d4fCJ8?si=ZS01d6Jk4ijPZmxa" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      <iframe className="react-player rounded-2xl" width="560" height="315" src="https://www.youtube.com/embed/N9Rk3d4fCJ8?si=ZS01d6Jk4ijPZmxa" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
       </div>
     </section>
   )
