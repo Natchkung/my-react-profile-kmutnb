173321e2d2019ad75bac75befb315110c4cb161b
Natchkung <natchnon654321@gmail.com>
2023-09-17 19:51:29 +0700

    update

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,14 +1,14 @@
 import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
-import model from '../../assets/stylized_planet.glb';
+
 import CanvasLoader from "../Loader";
 
 const Earth = () => {
-  const {scene} = useGLTF(model);
+  const earth = useGLTF("./planet/scene.gltf");
 
   return (
-    <primitive object= {scene} scale={3.5} position-y={0} rotation-y={0} />
+    <primitive object= {earth.scene} scale={3.5} position-y={0} rotation-y={0} />
   );
 };
 
